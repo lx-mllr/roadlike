dab6bd330b592e4878bf24ebc3acb0210469eee0
Alex Miller <lxmllr71@gmail.com>
2018-08-02 17:32:35 -0700

    Tiles: ResetComps to prefabs, Coin spawn to Views

--- Assets/Scripts/Grid/Tiles/Tile.cs
+++ Assets/Scripts/Grid/Tiles/Tile.cs
@@ -5,18 +5,29 @@ using Zenject;
 
 public class Tile : MonoBehaviour {
 
+	[Inject]
+	CoinView.Factory _coinFactory;
+
 	// Use this for initialization
 	public Vector3 nextGridPos;
 	public Vector3 nextTileEuler;
 
 	public MeshCollider meshCollider;
 
-	protected void Awake () {
+	private CoinView _coin;
+
+	public void SpawnCoin () {
+		_coin = _coinFactory.Create();
+		Vector3 tSize = meshCollider.bounds.size;
+		Vector3 pPos = transform.position + (UnityEngine.Random.Range(-0.7f, 0.7f) * tSize);
+		pPos.y = 1;
+		_coin.transform.position = pPos;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnDestroy () {
+		if (_coin && _coin.gameObject) {
+			Destroy(_coin.gameObject);
+		}
 	}
 
 	public class Factory : PlaceholderFactory<Tile>
