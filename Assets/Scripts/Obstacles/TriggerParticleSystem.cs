1facb5bc07b6294c9210aae3a627f6c93f7169df
Alex Miller <lxmllr71@gmail.com>
2018-09-05 18:31:40 -0700

    reorg folders

---
+++ Assets/Scripts/Obstacles/TriggerParticleSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerParticleSystem : MonoBehaviour {
+
+	public ParticleSystem _particles;
+
+	void OnTriggerEnter(Collider other) {
+		_particles.Play();
+	}
+}
